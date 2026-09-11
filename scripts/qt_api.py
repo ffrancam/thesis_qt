5c42cc401c14d33e5be7b63158676e3725b77a07
QTrobot <>
2026-09-11 14:32:53 +0200

    LOG

--- scripts/qt_api.py
+++ scripts/qt_api.py
@@ -112,7 +112,7 @@ class QTRobot:
 
   def configure_voice(self):
     self.speechConfig(language=self.tts_voice, pitch=115, speed=102)
-    self.setVolume(90)
+    self.setVolume(80)
 
 
   def hide_image(self):
